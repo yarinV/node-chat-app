e2387114a6a4d36d4b5a533495dad69b5c54334a
yarinV <Yarinva@888holdings.corp>
2017-11-30 12:42:56 +0200

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -9,28 +9,42 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-io.on('connection', (socket)=>{
+io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.on('disconnect', ()=>{
-    console.log('user was disconnected');
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
   });
 
-  socket.on('createMessage', (message)=>{
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user connected to chat'
+  })
+
+  socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage',{
+    io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage',{
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
+  socket.on('disconnect', () => {
+    console.log('user was disconnected');
+  });
 
 });
 
 
 app.use(express.static(publicPath));
 
-server.listen(port, ()=>{
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 })
